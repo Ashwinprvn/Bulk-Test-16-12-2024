52a54bfd68d349bfbab53bb844754679f4d28458
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-17 10:48:12 +0530

    body.tsx

--- src/app/components/body.tsx
+++ src/app/components/body.tsx
@@ -6,15 +6,15 @@ const Body = () => {
         <div className="flex flex-row justify-evenly">
 
           <div className="ml-2">
-            <h1 className="font-tafel text-blue-900 text-xxs">INCREDIENTS</h1>
-            <h1 className="font-tafel text-black text-sm truncate">Better Incredients</h1>
+            <h1 className="font-bold text-blue-900 text-xxs">INCREDIENTS</h1>
+            <h1 className="font-bold text-black text-sm truncate">Better Incredients</h1>
             <h1 className=" text-black text-xxxs truncate mt-4"> Only the best when you choose products offered on our platform - high-quality</h1>
             <h1 className=" text-black text-xxxs truncate">ingredients for high quality products!</h1>
           </div>
 
           <div className="w-[110px] h-[70px] bg-[url('/vitamin-c.png')] bg-cover bg-center rounded-lg flex-col justify-between ">
             <div>
-              <h1 className="font-tafel text-black text-xxs mt-2 truncate ml-1">Vitamin C</h1>
+              <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin C</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Vitamin C as ascorbic acid</h1>
             </div>
             <a href="https://ods.od.nih.gov/factsheets/VitaminC-HealthProfessional/#:~:text=Vitamin%20C%2C%20also%20known%20as,essential%20dietary%20component%20%5B1%5D."  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>  
@@ -22,7 +22,7 @@ const Body = () => {
 
           <div className="w-[110px] h-[70px] bg-[url('/bn2-2.jpg.png')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
-              <h1 className="font-tafel text-black text-xxs mt-2 truncate ml-1">Vitamin B3</h1>
+              <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin B3</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Niacin for healthy gut and skin</h1>
             </div>
             <a href="https://www.webmd.com/diet/supplement-guide-niacin"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
@@ -33,7 +33,7 @@ const Body = () => {
 
         <div className="w-[110px] h-[70px] bg-[url('/magnesium.png')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
-              <h1 className="font-tafel text-black text-xxs mt-2 truncate ml-1">Magnesium</h1>
+              <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Magnesium</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Boost energy and support</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">muscle function</h1>
             </div>
@@ -42,7 +42,7 @@ const Body = () => {
 
            <div className="w-[110px] h-[70px] bg-[url('/hyaluronic.png')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
-              <h1 className="font-tafel text-black text-xxs mt-2 truncate ml-1">Hyaluronic Acid</h1>
+              <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Hyaluronic Acid</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">For smooth,</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">supple and soft skin!</h1>
             </div>
@@ -51,7 +51,7 @@ const Body = () => {
 
           <div className="w-[110px] h-[70px] bg-[url('/lactobacilus.png')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
-              <h1 className="font-tafel text-black text-xxs mt-2 truncate ml-1">Lactobaculus</h1>
+              <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Lactobaculus</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Invigorate your gut</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">microbiome</h1>
             </div>

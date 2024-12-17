440dd7f05ea9f30cdf23f9c6730f02bec3e9f55c
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-17 11:18:21 +0530

    body.tsx

--- src/app/components/body.tsx
+++ src/app/components/body.tsx
@@ -12,7 +12,7 @@ const Body = () => {
             <h1 className=" text-black text-xxxs truncate">ingredients for high quality products!</h1>
           </div>
 
-          <div className="w-[110px] h-[70px] bg-[url('/vitamin-c.png')] bg-cover bg-center rounded-lg flex-col justify-between ">
+          <div className="w-[110px] h-[70px] bg-[url('/vitamin-c.webp')] bg-cover bg-center rounded-lg flex-col justify-between ">
             <div>
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin C</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Vitamin C as ascorbic acid</h1>
@@ -20,7 +20,7 @@ const Body = () => {
             <a href="https://ods.od.nih.gov/factsheets/VitaminC-HealthProfessional/#:~:text=Vitamin%20C%2C%20also%20known%20as,essential%20dietary%20component%20%5B1%5D."  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>  
           </div>
 
-          <div className="w-[110px] h-[70px] bg-[url('/bn2-2.jpg.png')] bg-cover bg-center rounded-lg flex-col justify-between">
+          <div className="w-[110px] h-[70px] bg-[url('/bn2-2.jpg.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin B3</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Niacin for healthy gut and skin</h1>
@@ -31,7 +31,7 @@ const Body = () => {
 
         <div className="flex flex-row justify-evenly mt-2 mb-4">
 
-        <div className="w-[110px] h-[70px] bg-[url('/magnesium.png')] bg-cover bg-center rounded-lg flex-col justify-between">
+        <div className="w-[110px] h-[70px] bg-[url('/magnesium.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Magnesium</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Boost energy and support</h1>
@@ -40,7 +40,7 @@ const Body = () => {
             <a href="https://ods.od.nih.gov/factsheets/Magnesium-HealthProfessional/#:~:text=Magnesium%20is%20a%20cofactor%20in,%2C%20oxidative%20phosphorylation%2C%20and%20glycolysis."  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
 
-           <div className="w-[110px] h-[70px] bg-[url('/hyaluronic.png')] bg-cover bg-center rounded-lg flex-col justify-between">
+           <div className="w-[110px] h-[70px] bg-[url('/hyaluronic.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Hyaluronic Acid</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">For smooth,</h1>
@@ -49,7 +49,7 @@ const Body = () => {
             <a href="https://www.webmd.com/vitamins/ai/ingredientmono-1062/hyaluronic-acid"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
 
-          <div className="w-[110px] h-[70px] bg-[url('/lactobacilus.png')] bg-cover bg-center rounded-lg flex-col justify-between">
+          <div className="w-[110px] h-[70px] bg-[url('/lactobacilus.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
             <div>
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Lactobaculus</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Invigorate your gut</h1>
@@ -57,7 +57,7 @@ const Body = () => {
             </div>
             <a href="https://www.britannica.com/science/Lactobacillus"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
-          <Image src="/color.png" height={40} width={40} alt="img" loading="lazy"/>
+          <Image src="/color.webp" height={40} width={40} alt="img" loading="lazy"/>
         </div>
       </div>
 )

13d60f959f81696ecd06f9b55165bfd702cd1b16
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-17 11:38:13 +0530

    blog.tsx

--- src/app/components/blog.tsx
+++ src/app/components/blog.tsx
@@ -41,11 +41,11 @@ const Blog = () => {
             </div>
           </div>
           <div >
-            <div className="w-[110px] h-[70px] bg-[url('/top.webp)] bg-cover bg-center rounded-lg flex-col ">
+            <div className="w-[110px] h-[70px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col ">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
-            <div className="w-[110px] h-[150px] bg-[url('/bottom.wepb')] bg-cover bg-center rounded-lg flex-col mt-2">
+            <div className="w-[110px] h-[150px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">Hac hendrerit mus nons semper suspendisse</p>
             </div>

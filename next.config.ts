7a277b51a6cae52707fcf847daea8a4c478618ec
Ashwinprvn <ashwinprvn@gmail.com>
2024-12-17 10:16:14 +0530

    Resolved merge conflicts

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  images: {
+    unoptimized: true, // Disables optimization for static builds
+  },
+};
+
+
 export default nextConfig;

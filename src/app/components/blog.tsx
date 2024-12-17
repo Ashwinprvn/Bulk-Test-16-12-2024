7f93fee405404f026e965333bebc521c38d42739
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-17 11:11:12 +0530

    blog.tsx

--- src/app/components/blog.tsx
+++ src/app/components/blog.tsx
@@ -5,47 +5,47 @@ const Blog = () => {
         <h1 className="text-md font-bold text-green-900 ">Latest News</h1>
         <div className="flex flex-row justify-evenly mt-4 space-x-1">
           <div >
-            <div className="w-[110px] h-[150px] bg-[url('/top.png')] bg-cover bg-center rounded-lg flex-col">
+            <div className="w-[110px] h-[150px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col">
               <div >
                 <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
                 <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
               </div>
           
             </div>
-            <div className="w-[110px] h-[70px] bg-[url('/bottom.png')] bg-cover bg-center rounded-lg flex-col mt-2">
+            <div className="w-[110px] h-[70px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
           </div>
           <div >
-            <div className="w-[95px] h-[70px] bg-[url('/top.png')] bg-cover bg-center rounded-lg flex-col ">
+            <div className="w-[95px] h-[70px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col ">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
-            <div className="w-[95px] h-[150px] bg-[url('/bottom.png')] bg-cover bg-center rounded-lg flex-col mt-2">
+            <div className="w-[95px] h-[150px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
           </div>
           <div >
-            <div className="w-[95px] h-[150px] bg-[url('/top.png')] bg-cover bg-center rounded-lg flex-col">
+            <div className="w-[95px] h-[150px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col">
               <div >
                 <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
                 <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
               </div>
           
             </div>
-            <div className="w-[95px] h-[70px] bg-[url('/bottom.png')] bg-cover bg-center rounded-lg flex-col mt-2">
+            <div className="w-[95px] h-[70px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
           </div>
           <div >
-            <div className="w-[110px] h-[70px] bg-[url('/top.png')] bg-cover bg-center rounded-lg flex-col ">
+            <div className="w-[110px] h-[70px] bg-[url('/top.webp)] bg-cover bg-center rounded-lg flex-col ">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
             </div>
-            <div className="w-[110px] h-[150px] bg-[url('/bottom.png')] bg-cover bg-center rounded-lg flex-col mt-2">
+            <div className="w-[110px] h-[150px] bg-[url('/bottom.wepb')] bg-cover bg-center rounded-lg flex-col mt-2">
             <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
             <p className="text-white text-xxs font-bold p-1">Hac hendrerit mus nons semper suspendisse</p>
             </div>

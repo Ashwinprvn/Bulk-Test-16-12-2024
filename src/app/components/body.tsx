84b5eeebc63a8d90510e16e65920cbb3355feeaf
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-19 06:04:21 +0530

    body.tsx

--- src/app/components/body.tsx
+++ src/app/components/body.tsx
@@ -17,7 +17,7 @@ const Body = () => {
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin C</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Vitamin C as ascorbic acid</h1>
             </div>
-            <a href="https://ods.od.nih.gov/factsheets/VitaminC-HealthProfessional/#:~:text=Vitamin%20C%2C%20also%20known%20as,essential%20dietary%20component%20%5B1%5D."  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>  
+            <a href="#"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>  
           </div>
 
           <div className="w-[110px] h-[70px] bg-[url('/bn2-2.jpg.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
@@ -25,7 +25,7 @@ const Body = () => {
               <h1 className="font-bold text-black text-xxs mt-2 truncate ml-1">Vitamin B3</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">Niacin for healthy gut and skin</h1>
             </div>
-            <a href="https://www.webmd.com/diet/supplement-guide-niacin"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
+            <a href="#"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
         </div>
 
@@ -37,7 +37,7 @@ const Body = () => {
               <h1 className=" text-black text-xxxs truncate ml-1">Boost energy and support</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">muscle function</h1>
             </div>
-            <a href="https://ods.od.nih.gov/factsheets/Magnesium-HealthProfessional/#:~:text=Magnesium%20is%20a%20cofactor%20in,%2C%20oxidative%20phosphorylation%2C%20and%20glycolysis."  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
+            <a href="#"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
 
            <div className="w-[110px] h-[70px] bg-[url('/hyaluronic.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
@@ -46,7 +46,7 @@ const Body = () => {
               <h1 className=" text-black text-xxxs truncate ml-1">For smooth,</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">supple and soft skin!</h1>
             </div>
-            <a href="https://www.webmd.com/vitamins/ai/ingredientmono-1062/hyaluronic-acid"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
+            <a href="#"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
 
           <div className="w-[110px] h-[70px] bg-[url('/lactobacilus.webp')] bg-cover bg-center rounded-lg flex-col justify-between">
@@ -55,7 +55,7 @@ const Body = () => {
               <h1 className=" text-black text-xxxs truncate ml-1">Invigorate your gut</h1>
               <h1 className=" text-black text-xxxs truncate ml-1">microbiome</h1>
             </div>
-            <a href="https://www.britannica.com/science/Lactobacillus"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
+            <a href="#"  className=" text-blue-700 underline font-bold text-xxxs ml-1 mt-7">See More</a>
           </div>
           <Image src="/color.webp" height={40} width={40} alt="img" loading="lazy"/>
         </div>

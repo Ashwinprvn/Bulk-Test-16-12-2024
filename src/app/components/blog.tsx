e2267ec9e65034978dc20bd81ab49400e40a93e2
Ashwinprvn <132207436+Ashwinprvn@users.noreply.github.com>
2024-12-19 06:26:59 +0530

    blog.tsx

--- src/app/components/blog.tsx
+++ src/app/components/blog.tsx
@@ -1,58 +1,43 @@
-const Blog = () => {
-    return(
-        <div className="flex flex-col items-center">
-        <h1 className="font-sans font-bold text-sm text-blue-900">OUR BLOG</h1>
-        <h1 className="text-md font-bold text-green-900 ">Latest News</h1>
-        <div className="flex flex-row justify-evenly mt-4 space-x-1">
-          <div >
-            <div className="w-[110px] h-[150px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col">
-              <div >
-                <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-                <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-              </div>
-          
-            </div>
-            <div className="w-[110px] h-[70px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-            </div>
-          </div>
-          <div >
-            <div className="w-[95px] h-[70px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col ">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-            </div>
-            <div className="w-[95px] h-[150px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-            </div>
-          </div>
-          <div >
-            <div className="w-[95px] h-[150px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col">
-              <div >
-                <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-                <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-              </div>
-          
-            </div>
-            <div className="w-[95px] h-[70px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-            </div>
-          </div>
-          <div >
-            <div className="w-[110px] h-[70px] bg-[url('/top.webp')] bg-cover bg-center rounded-lg flex-col ">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">The Covid-19 Epidemic In 2023 Is Back</p>
-            </div>
-            <div className="w-[110px] h-[150px] bg-[url('/bottom.webp')] bg-cover bg-center rounded-lg flex-col mt-2">
-            <p className="text-white text-xxs rounded-r-3xl bg-blue-900 w-10">20 APR</p>
-            <p className="text-white text-xxs font-bold p-1">Hac hendrerit mus nons semper suspendisse</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-}
+import Image from "next/image";
 
-export default Blog
+const Blog = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="font-sans font-bold text-sm text-blue-900">OUR BLOG</h1>
+      <h1 className="text-md font-bold text-green-900">Latest News</h1>
+
+      <div className="flex flex-row justify-evenly mt-4 space-x-4">
+        {[
+          { img1: "/top.webp", img2: "/bottom.webp", text: "The Covid-19 Epidemic In 2023 Is Back" },
+          { img1: "/top.webp", img2: "/bottom.webp", text: "Hac hendrerit mus nons semper suspendisse" },
+        ].map((item, index) => (
+          <div key={index} className="flex flex-col space-y-2">
+            <div className="w-[110px] h-[150px] relative rounded-lg">
+              <Image 
+                src={item.img1} 
+                alt="Blog Top Image" 
+                layout="fill" 
+                objectFit="cover" 
+                className="rounded-lg"
+              />
+              <p className="absolute top-2 left-2 text-white text-xxs bg-blue-900 px-2 py-1 rounded-r-3xl">20 APR</p>
+            </div>
+            <div className="w-[110px] h-[70px] relative rounded-lg">
+              <Image 
+                src={item.img2} 
+                alt="Blog Bottom Image" 
+                layout="fill" 
+                objectFit="cover" 
+                className="rounded-lg"
+              />
+              <p className="absolute top-2 left-2 text-white text-xxs bg-blue-900 px-2 py-1 rounded-r-3xl">20 APR</p>
+              <p className="text-white text-xxs font-bold px-2">{item.text}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
